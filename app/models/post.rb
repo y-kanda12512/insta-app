e38210cfc7c85b9443818e4aa0a556e17b375a5b
yuto <y.kanda839yuzyou@gmail.com>
2024-10-30 11:30:54 +0900

    ログイン時間表示（間違えてた。。。）ではなく投稿時間を表示するよう修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,17 @@
 #
 #  index_posts_on_user_id  (user_id)
 #
+
+require 'time'
+
 class Post < ApplicationRecord
     belongs_to :user
     has_many_attached :post_images
+
+    def posting_hour(post_time)
+        current_time = Time.now
+        time_difference = ( current_time - post_time ) / 3600
+        time_difference_i = time_difference.to_i
+        "#{time_difference_i} hours ago"
+    end
 end
