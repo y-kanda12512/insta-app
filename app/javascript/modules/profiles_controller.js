fe2a99940fea376ecb73b5d2c0ca4c21c8698d02
yuto <y.kanda839yuzyou@gmail.com>
2024-11-04 11:46:31 +0900

    moduleファイルを必要に応じてファイル別に読み込む

---
+++ app/javascript/modules/profiles_controller.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const profileImage = document.getElementById("profileImage");
+
+  if (profileImage) {
+    profileImage.addEventListener("click", function () {
+      document.getElementById("imageUploadInput").click();
+    });
+
+    document.getElementById("imageUploadInput").addEventListener("change", function () {
+      document.getElementById("imageUploadForm").submit();
+    });
+  }
+});
