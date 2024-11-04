dd9428c0ae80dc985c0bdec47035780efc76e42b
yuto <y.kanda839yuzyou@gmail.com>
2024-11-04 10:00:19 +0900

    axios導入前準備

--- app/javascript/modules/handle_heart.js
+++ app/javascript/modules/handle_heart.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-import $ from "jquery";
-import { csrfToken } from "rails-ujs";
+// // import axios from "axios";
+// // import $ from "jquery";
+// // import { csrfToken } from "rails-ujs";
 
-// axiosにCSRFトークンを設定
-axios.defaults.headers.common["X-CSRF-Token"] = csrfToken();
+// // axiosにCSRFトークンを設定
+// // axios.defaults.headers.common["X-CSRF-Token"] = csrfToken();
 
-const handleHeartDisplay = (hasLiked) => {
-  if (hasLiked) {
-    $(".active-heart").removeClass("hidden");
-    console.log("hasLiked is true");
-  } else {
-    $(".inactive-heart").removeClass("hidden");
-    console.log("hasLiked is false");
-  }
-};
+// const handleHeartDisplay = (hasLiked) => {
+//   if (hasLiked) {
+//     $(".active-heart").removeClass("hidden");
+//     console.log("hasLiked is true");
+//   } else {
+//     $(".inactive-heart").removeClass("hidden");
+//     console.log("hasLiked is false");
+//   }
+// };
 
-document.addEventListener("turbolinks:load", () => {
-  const dataset = $("#post-show").data();
-  const postId = dataset.postId;
+// document.addEventListener("turbolinks:load", () => {
+//   const dataset = $("#post-show").data();
+//   const postId = dataset.postId;
 
-  //   ここでlikesコントローラーのshowメソッドが発動する
-  axios.get(`/posts/${postId}/like`).then((response) => {
-    const hasLiked = response.data.hasLiked;
-    handleHeartDisplay(hasLiked);
-  });
-});
+//   //   ここでlikesコントローラーのshowメソッドが発動する
+//   axios.get(`/posts/${postId}/like`).then((response) => {
+//     const hasLiked = response.data.hasLiked;
+//     handleHeartDisplay(hasLiked);
+//   });
+// });
